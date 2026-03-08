d60f324f0595570f4616369aab7d04cb7b9a8d3a
yazankh28 <yazankh0115@gmail.com>
2026-03-08 03:32:13 +0100

    Add files via upload

---
+++ Lab2/Lab2Tests/Lab2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2Tests.swift
+//  Lab2Tests
+//
+//  Created by Yazan Khraiba on 2026-03-08.
+//
+
+import Testing
+@testable import Lab2
+
+struct Lab2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Lab2/Lab2UITests/Lab2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Lab2UITestsLaunchTests.swift
+//  Lab2UITests
+//
+//  Created by Yazan Khraiba on 2026-03-08.
+//
+
+import XCTest
+
+final class Lab2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
